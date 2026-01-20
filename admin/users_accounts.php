6415a9712b8e9d6cb4795b41a1b1eacb96307251
kaneriyaNirmal <160276642+kaneriyaNirmal@users.noreply.github.com>
2026-01-20 15:39:46 +0530

    Add files via upload

--- admin/users_accounts.php
+++ admin/users_accounts.php
@@ -12,9 +12,13 @@ if(!isset($admin_id)){
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
-   $delete_admin = $conn->prepare("DELETE FROM `admin` WHERE id = ?");
-   $delete_admin->execute([$delete_id]);
-   header('location:admin_accounts.php');
+   $delete_users = $conn->prepare("DELETE FROM `users` WHERE id = ?");
+   $delete_users->execute([$delete_id]);
+   $delete_order = $conn->prepare("DELETE FROM `orders` WHERE user_id = ?");
+   $delete_order->execute([$delete_id]);
+   $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+   $delete_cart->execute([$delete_id]);
+   header('location:users_accounts.php');
 }
 
 ?>
@@ -25,7 +29,7 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>admins accounts</title>
+   <title>users accounts</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -38,36 +42,24 @@ if(isset($_GET['delete'])){
 
 <?php include '../components/admin_header.php' ?>
 
-<!-- admins accounts section starts  -->
+<!-- user accounts section starts  -->
 
 <section class="accounts">
 
-   <h1 class="heading">admins account</h1>
+   <h1 class="heading">users account</h1>
 
    <div class="box-container">
 
-   <div class="box">
-      <p>register new admin</p>
-      <a href="register_admin.php" class="option-btn">register</a>
-   </div>
-
    <?php
-      $select_account = $conn->prepare("SELECT * FROM `admin`");
+      $select_account = $conn->prepare("SELECT * FROM `users`");
       $select_account->execute();
       if($select_account->rowCount() > 0){
          while($fetch_accounts = $select_account->fetch(PDO::FETCH_ASSOC)){  
    ?>
    <div class="box">
-      <p> admin id : <span><?= $fetch_accounts['id']; ?></span> </p>
+      <p> user id : <span><?= $fetch_accounts['id']; ?></span> </p>
       <p> username : <span><?= $fetch_accounts['name']; ?></span> </p>
-      <div class="flex-btn">
-         <a href="admin_accounts.php?delete=<?= $fetch_accounts['id']; ?>" class="delete-btn" onclick="return confirm('delete this account?');">delete</a>
-         <?php
-            if($fetch_accounts['id'] == $admin_id){
-               echo '<a href="update_profile.php" class="option-btn">update</a>';
-            }
-         ?>
-      </div>
+      <a href="users_accounts.php?delete=<?= $fetch_accounts['id']; ?>" class="delete-btn" onclick="return confirm('delete this account?');">delete</a>
    </div>
    <?php
       }
@@ -80,20 +72,7 @@ if(isset($_GET['delete'])){
 
 </section>
 
-<!-- admins accounts section ends -->
-
-
-
-
-
-
-
-
-
-
-
-
-
+<!-- user accounts section ends -->
 
 
 
